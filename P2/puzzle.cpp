994b827d84daff22cb3f4e0ff0be3ee2d9997b8b
ymirthor <ymirthorleifsson@gmail.com>
2020-09-14 19:25:15 +0000

    started on puzzle

---
+++ P2/puzzle.cpp
@@ -0,0 +1,102 @@
+/* 
+========================================================================
+                                 PUZZLE
+========================================================================
+                                 Authors:
+                           Kristofer Robertsson
+                            Ymir Thorleifsson
+========================================================================
+Messages we got:
+
+PORT 4042:
+
+I am the oracle, send me a comma-seperated list of the hidden ports, 
+and I shall show you the way.
+
+PORT 4097:  
+
+The dark side of network programming is a pathway to many abilities 
+some consider to be...unnatural. (https://en.wikipedia.org/wiki/Evil_bit)
+Send us a message containing your group number in the form "$group_#$" 
+where # is your group number.
+
+PORT 4099:  
+
+My boss told me not to tell anyone that my secret port is 4002
+
+*/
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+#include <iostream>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+
+int main(int argc, char* argv[]) {
+    if(argc != 1) {
+        printf("Usage: ./puzzle \n");
+        exit(0);
+    }
+
+    int sock;
+    struct timeval tv;
+    
+    char buffer[1024];                                 
+    char msg[16];
+    strcpy(msg, "knock");
+    char * port;
+
+    for (int i = 0; i < 3; i++) {
+        // if (i == 0) {
+        //     char * port = "4042";
+        // } else if (i == 1) {
+        //     char * port = "4097";
+        // } else {
+        //     char * port = "4099";
+        // }
+        char * port = "4042";
+        printf("Message from port %s:\n", port);
+
+
+        if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+            perror("Failed to make socket");
+            close(sock);
+            exit(-1);
+        }
+
+        struct sockaddr_in server_address;
+        server_address.sin_family       = AF_INET;
+        server_address.sin_port         = htons(atoi(port));
+        server_address.sin_addr.s_addr  = inet_addr("130.208.243.61"); //skel.ru.is
+
+        if (sendto(sock, msg, sizeof(msg), 0x0, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+            perror("failed to send");
+            exit(-1);
+        }
+
+        
+        tv.tv_sec = 1;
+        setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+        if (recvfrom(sock, buffer, sizeof(buffer), 0x0, NULL, NULL) < 0) {
+            printf("CLOSED\n");
+            close(sock);
+            continue;
+        } 
+
+        printf("%s", buffer);
+        for (int i = 0; i < (int) sizeof(buffer); i++) {
+            buffer[i] = '\0';
+        }
+        close(sock);
+        }
+    printf("\n");
+}
--- P2/scanner.cpp
+++ P2/scanner.cpp
@@ -1,3 +1,17 @@
+/* 
+=====================================
+            PORT SCANNER
+=====================================
+              Authors:
+        Kristofer Robertsson
+          Ymir Thorleifsson
+=====================================
+  - 60% of the time it works
+    every time 
+
+*/
+
+
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
@@ -20,27 +34,25 @@ int main(int argc, char* argv[]) {
                " <low port> <high port>\n");
         exit(0);
     }
-
     /* Inputs */
-    char*   ip_addr     = argv[1];                             
-    int     port_low;   sscanf(argv[2], "%d", &port_low);   
-    int     port_high;  sscanf(argv[3], "%d", &port_high);   
+    char* ip_addr = argv[1];                             
+    int port_low;   sscanf(argv[2], "%d", &port_low);   
+    int port_high;  sscanf(argv[3], "%d", &port_high);   
     
-    /* Socket */
-    int     sock;
-    struct  timeval tv;
+    int sock;
+    struct timeval tv;
 
-    /* Buffers */
-    char    buffer[1024];                                 
-    char    msg[16];
-    strcpy(msg, "gaman");
+    char buffer[1024];                                 
+    char msg[16];
+    strcpy(msg, "knock");
     
-    printf("Port scanning IP %s, from ports %d to %d", 
+    printf("Port scanning IP %s, from ports %d to %d\n", 
             ip_addr, port_low, port_high);
-    
+
     for (int port = port_low; port <= port_high; port++) {
 
-        printf("\nSending UDP to port %d:  ", port);
+        printf("Sending UDP to port %d: ", port);
+
 
         if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
             perror("Failed to make socket");
@@ -58,20 +70,21 @@ int main(int argc, char* argv[]) {
             exit(-1);
         }
 
-        int nrbytes;
         
-        tv.tv_usec = 500;
+        tv.tv_sec = 1;
         setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
-        nrbytes = recvfrom(sock, buffer, sizeof(buffer), 0x0, NULL, NULL);
+        if (recvfrom(sock, buffer, sizeof(buffer), 0x0, NULL, NULL) < 0) {
+            printf("CLOSED\n");
+            close(sock);
+            continue;
+        } 
 
-        printf("%s", buffer);
-        if (nrbytes > 1) {
-            for (int i = 0; i < sizeof(buffer); i++) {
-                buffer[i] = '\0';
-            }
+        printf("OPEN\n");
+        // printf("%s", buffer);
+        for (int i = 0; i < (int) sizeof(buffer); i++) {
+            buffer[i] = '\0';
         }
-        
         close(sock);
-    }
+        }
     printf("\n");
 }
