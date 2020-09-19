dd1f0a9897d8bb232d8e2b2dfeced024ffbae52d
ymir <ymirthorleifsson@gmail.com>
2020-09-19 16:01:25 +0000

    real stuffs

--- P2/puzzle.cpp
+++ P2/puzzle.cpp
@@ -27,20 +27,12 @@ My boss told me not to tell anyone that my secret port is 4002
 */
 
 #include <sys/socket.h>
-#include <sys/types.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <netinet/in.h>
-
-#include <iostream>
+#include <arpa/inet.h>
 #include <string.h>
+#include <string>
 
-#include <sys/socket.h>
-#include <netinet/ip.h>
-#include <netinet/udp.h>
-
+#include <stdio.h>
 #include <fstream>
 
 int main(int argc, char* argv[]) {
@@ -48,53 +40,68 @@ int main(int argc, char* argv[]) {
         printf("Usage: ./puzzle \n");
         exit(0);
     }
+    char* address = "130.208.243.61";
+    int low =  4000;
+    int high = 4100;
+    std::string command;
 
-    int sock;
-    struct timeval tv;
-    
-    char buffer[1024];                                 
-    char msg[16];
-    strcpy(msg, "knock");
+    printf("Running port scanner: ");
 
-    std::ifstream openports("open_ports.txt");
-    
+    command += "./scanner ";
+    command += address;
+    command += " ";
+    command += std::to_string(low);
+    command += " ";
+    command += std::to_string(high);
+    puts(command.c_str());
+    system(command.c_str());
+    printf("\nMessages:\n");
+
+    int udp_sock;
+    char buffer[1024];
+    int length;
     int port;
 
+    struct sockaddr_in destaddr;
+    struct timeval timeout;
+    std::ifstream openports("open_ports.txt");
+
+
+    /*  */
+    if ((udp_sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Unable to open socket");
+        return(-1);
+    }
+    /*  */
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 1;
+    setsockopt(udp_sock, SOL_SOCKET, SO_RCVTIMEO, (const char*) &timeout, sizeof(timeout));
+
+    /*  */
+    destaddr.sin_family = AF_INET;
+    inet_aton(address, &destaddr.sin_addr);
+    // inet_aton("130.208.243.61", &destaddr.sin_addr);
+
     while (openports >> port) {
+        /*  */
+        destaddr.sin_port = htons(port);
 
-        if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
-            perror("Failed to make socket");
-            close(sock);
-            exit(-1);
+        /*  */
+        strcpy(buffer, "knock");
+        length = strlen(buffer) + 1;
+
+        /*  */
+        if (sendto(udp_sock, buffer, length, 0x0, (const struct sockaddr *) &destaddr, sizeof(destaddr)) < 0) {
+            printf("Failed to send to port %d", port);
+            perror("");
         }
 
-        struct sockaddr_in server_address;
-        server_address.sin_family       = AF_INET;
-        server_address.sin_port         = htons(port);
-        server_address.sin_addr.s_addr  = inet_addr("130.208.243.61"); //skel.ru.is
-
-        if (sendto(sock, msg, sizeof(msg), 0x0, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
-            perror("failed to send");
-            exit(-1);
-        }
-
+        bzero(buffer, length); //
         
-        tv.tv_sec = 1;
-        setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
-
-        int msg_len = recvfrom(sock, buffer, sizeof(buffer), 0x0, NULL, NULL);
-
-        if (msg_len < 0) {
-            printf("Nothing\n");
-            close(sock);
-            continue;
-        } 
-
-        printf("Port %d: %s", port, buffer);
-        for (int i = 0; i < (int) sizeof(buffer); i++) {
-            buffer[i] = '\0';
+        /*  */
+        if (length = recvfrom(udp_sock, buffer, sizeof(buffer), 0x0, NULL, NULL) > 0) {
+            printf("Port %d:\n%s\n\n", port, buffer);
+            bzero(buffer, sizeof(buffer)); //
         }
-        close(sock);
-        }
-    printf("\n");
+    }
 }
