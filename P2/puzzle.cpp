73365c58c3d6acca3cd542c43fc430a32b1f4efb
ymirthor <ymirthorleifsson@gmail.com>
2020-09-22 23:35:12 +0000

    trying to do checksum

--- P2/puzzle.cpp
+++ P2/puzzle.cpp
@@ -41,6 +41,117 @@ My boss told me not to tell anyone that my secret port is 4002
 
 #include "scanner_utils.h"  //our utilty module
 
+void checksum_part(char* buffer, int buffersize, sockaddr_in destaddr, int port) {
+    int raw_sock;
+    timeval timeout;
+
+
+    /* Create raw socket */
+    if ((raw_sock = socket(AF_INET, SOCK_RAW, IPPROTO_UDP)) < 0) {
+        perror("Unable to open raw socket, did you forget to run with sudo?");
+        return;
+    }
+
+    /* Set timeout */
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 100;
+
+    /* Set options RAW */
+    setsockopt(raw_sock, SOL_SOCKET, SO_RCVTIMEO, (const char*) &timeout, sizeof(timeout));
+
+     int opt = 1;
+    if (setsockopt(raw_sock, IPPROTO_IP, IP_HDRINCL, &opt, sizeof(opt)) < 0) {
+        perror("Setting IP_HDRINCL failed");
+        return;
+    }
+
+    std::string message = std::string(buffer);
+    size_t pos = message.find("0x");
+    
+    char checksum[6 + 1]; // Guessing it needs +1 for '\0' 
+
+    if (pos == std::string::npos) {
+        return;
+    }
+    for (int i = 0; i < 6; i++) {
+        checksum[i] = buffer[i + (int) pos];
+    }
+
+    printf("Sending message with checksum...\n");
+
+    bzero(buffer, sizeof(buffer)); // Clear buffer
+
+    /* IP header */
+    iphdr *ipheader = (iphdr *) buffer;
+    
+    /* UDP header */
+    udphdr *udpheader = (udphdr *) (sizeof(ip) + buffer);
+    
+    /* Pseudo header */
+    pseudo_header pheader;
+    
+    /* Data */
+    char *data = sizeof(iphdr) + sizeof(udphdr) + buffer;
+    strcpy(data, "test");
+
+    /* Source IP */
+    char sourceip[32] = "192.168.1.17";
+
+    ipheader -> ihl     = 5;
+    ipheader -> version = 4;
+
+    ipheader -> tos     = 0;
+    ipheader -> tot_len = sizeof(iphdr) + sizeof(udphdr) + strlen(data);
+    ipheader -> id      = htonl(69696);
+    ipheader -> ttl     = 255;
+    ipheader -> protocol= IPPROTO_UDP;
+    ipheader -> check   = 0;
+    ipheader -> saddr   = inet_addr(sourceip);
+    ipheader -> daddr   = destaddr.sin_addr.s_addr;
+
+    ipheader -> check   = csum((u_short *) buffer, ipheader->tot_len);
+
+    udpheader-> source  = htons(ipheader -> saddr);
+    udpheader-> dest    = htons(port);
+    udpheader-> len     = htons(8 + strlen(data));
+    udpheader-> check   = 0;
+
+    pheader.source      = inet_addr(sourceip);
+    pheader.dest        = destaddr.sin_addr.s_addr;
+    pheader.zeros       = 0;
+    pheader.udp_length  = htons(sizeof(udphdr) + strlen(data));
+
+    int psize = sizeof(pseudo_header) + sizeof(udphdr) + strlen(data);
+    char *pseudogram[psize];
+
+    memcpy(pseudogram, (char *) &pheader, sizeof(pseudo_header));
+    memcpy(pseudogram + sizeof(pseudo_header), udpheader, sizeof(udphdr) + strlen(data));
+
+    //udpheader-> check   = csum((u_short *) pseudogram, psize);
+
+    u_int16_t old_check = std::stoi(checksum, 0, 16);
+
+    u_int16_t new_check = (old_check>>8) + (old_check<<8);
+
+    udpheader-> check   = new_check; // What we got from the message
+
+    printf("Sending with checksum: %x\n", udpheader -> check);
+    /* Send message to buffer */
+    if (sendto(raw_sock, buffer, ipheader->tot_len, 0x0, (struct sockaddr *) &destaddr, sizeof(destaddr)) < 0) {
+        perror("Failed to send package");
+    }
+
+    /* Get response from server */
+    bzero(buffer, sizeof(buffer)); // Clear buffer
+    int length = recvfrom(raw_sock, buffer, sizeof(buffer), 0x0, NULL, NULL);
+    if (length > 0) {
+        printf("Got: %s \n", buffer);
+    } else {
+        perror("Failed to recv package");
+    }
+}
+
+
 int main(int argc, char* argv[]) {
     if(argc != 1) {
         printf("Usage: ./puzzle \n");
@@ -51,7 +162,7 @@ int main(int argc, char* argv[]) {
     int     low =  4000;
     int     high = 4100;
 
-    int     udp_sock, raw_sock;
+    int     udp_sock;
     char    buffer[1024];
     std::string message;
 
@@ -63,11 +174,6 @@ int main(int argc, char* argv[]) {
         perror("Unable to open socket");
         return -1;
     }
-    /* Create raw socket */
-    if ((raw_sock = socket(AF_INET, SOCK_RAW, IPPROTO_UDP)) < 0) {
-        perror("Unable to open raw socket, did you forget to run with sudo?");
-        return -1;
-    }
 
     /* Set timeout */
     timeout.tv_sec = 0;
@@ -76,16 +182,7 @@ int main(int argc, char* argv[]) {
     /* Set options UDP */
     setsockopt(udp_sock, SOL_SOCKET, SO_RCVTIMEO, (const char*) &timeout, sizeof(timeout));
 
-    /* Set options RAW */
-    setsockopt(raw_sock, SOL_SOCKET, SO_RCVTIMEO, (const char*) &timeout, sizeof(timeout));
-
-    
-
-    int opt = 1;
-    if (setsockopt(raw_sock, IPPROTO_IP, IP_HDRINCL, &opt, sizeof(opt)) < 0) {
-        perror("Setting IP_HDRINCL failed");
-        return -1;
-    }
+   
 
     /* Set destination address */
     destaddr.sin_family = AF_INET;
@@ -96,84 +193,7 @@ int main(int argc, char* argv[]) {
         int length = send_to_server(buffer, sizeof(buffer), (char*) "$group_42$", udp_sock, destaddr, port);
         if (length) {
             printf("%d: Message : %s\n", port, buffer);
-            
-            message = std::string(buffer);
-            size_t pos = message.find("0x");
-            char checksum[6 + 1]; // Guessing it needs +1 for '\0' 
-
-            if (pos != std::string::npos) {
-                for (int i = 0; i < 6; i++) {
-                    checksum[i] = buffer[i + (int) pos];
-                }
-                int num = std::stoi(checksum, 0, 16);
-            
-                printf("Sending message with checksum...\n", num);
-
-                bzero(buffer, sizeof(buffer)); // Clear buffer
-
-                /* IP header */
-                iphdr *ipheader = (iphdr *) buffer;
-                
-                /* UDP header */
-                udphdr *udpheader = (udphdr *) (sizeof(ip) + buffer);
-                
-                /* Pseudo header */
-                pseudo_header pheader;
-                
-                /* Data */
-                char *data = sizeof(iphdr) + sizeof(udphdr) + buffer;
-                strcpy(data, "test");
-
-                /* Source IP */
-                char sourceip[32] = "192.168.1.17";
-
-                ipheader -> ihl     = 5;
-                ipheader -> version = 4;
-
-                ipheader -> tos     = 0;
-                ipheader -> tot_len = sizeof(iphdr) + sizeof(udphdr) + strlen(data);
-                ipheader -> id      = htonl(69696);
-                ipheader -> ttl     = 255;
-                ipheader -> protocol= IPPROTO_UDP;
-                ipheader -> check   = 0;
-                ipheader -> saddr   = inet_addr (sourceip);
-                ipheader -> daddr   = destaddr.sin_addr.s_addr;
-
-                ipheader -> check   = csum((u_short *) buffer, ipheader->tot_len);
-
-                udpheader-> source  = htons(0);
-                udpheader-> dest    = htons(port);
-                udpheader-> len     = htons(8 + strlen(data));
-                udpheader-> check   = 0;
-
-                pheader.source      = inet_addr(sourceip);
-                pheader.dest        = destaddr.sin_addr.s_addr;
-                pheader.zeros       = 0;
-                pheader.udp_length  = htons(sizeof(udphdr) + strlen(data));
-
-                int psize = sizeof(pseudo_header) + sizeof(udphdr) + strlen(data);
-                char *pseudogram[psize];
-
-                memcpy(pseudogram, (char *) &pheader, sizeof(pseudo_header));
-                memcpy(pseudogram + sizeof(pseudo_header), udpheader, sizeof(udphdr) + strlen(data));
-
-                udpheader-> check   = csum((u_short *) pseudogram, psize);
-
-                printf("Sending with checksum: %x\n", udpheader -> check);
-                /* Send message to buffer */
-                if (sendto(raw_sock, buffer, ipheader->tot_len, 0x0, (struct sockaddr *) &destaddr, sizeof(destaddr)) < 0) {
-                    perror("Failed to send package");
-                }
-
-                /* Get response from server */
-                bzero(buffer, sizeof(buffer)); // Clear buffer
-                length = recvfrom(raw_sock, buffer, sizeof(buffer), 0x0, NULL, NULL);
-                if (length > 0) {
-                    printf("Got: %s \n", buffer);
-                } else {
-                    perror("Failed to recv package");
-                }
-            }
+            checksum_part(buffer, sizeof(buffer), destaddr, port);
         }
     }
 }
